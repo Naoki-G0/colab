ab9ea124d0a6bee998972009d35ea0778d243ca6
Naoki-G0 <nlabg1234@gmail.com>
2019-11-08 20:04:48 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+ <div>
+  <h1>Hello World of Programming!</h1>
+  <NLink to="/about">
+   About Page
+  </NLink>
+  <br>
+  <NLink to="/profile">
+   Profile
+  </NLink>
+ </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
---
+++ pages/profile.vue
@@ -0,0 +1,8 @@
+<template>
+ <div>
+  <h1>My profile</h1>
+  <NLink to="/">
+   Top Page
+  </NLink>
+ </div>
+</template>
